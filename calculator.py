24e54115c2e33d868e7a271f8f3563cdb76c426f
kanhaiya-agrawal-ey <balji7138@gmail.com>
2025-11-28 13:00:18 +0000

    Added multiply and division.

--- calculator.py
+++ calculator.py
@@ -4,6 +4,17 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if( b != 0):
+        return a / b
+    return "Division by zero."
+
+
 if __name__ == "__main__":
     print("Add:", add(5, 3))
     print("Subtract:", subtract(5, 3))
+    print("Multiply:", mul(5, 3))
+    print("Division:", div(5, 3))
